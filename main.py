a3cb6c558c7624cfb3b0b775abecd62a2c536e27
eduardotorresdev <admin@eduardotorres.dev>
2020-12-09 17:45:36 -0300

    feat: criação dos dataset no tensorflow

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
+import numpy as np
+import tensorflow as tf
 import csv
-
-
-class Jogador(object):
-    def __init__(self, dados):
-        self.id = dados[0]
-        self.idade = dados[1]
-        self.altura = dados[2]
-        self.tecnica = dados[3]
-        self.passe = dados[4]
-        self.chute = dados[5]
-        self.forca = dados[6]
-        self.velocidade = dados[7]
-        self.drible = dados[8]
-        self.classe = None if len(dados) != 10 else dados[9]
-
-    def __str__(self):
-        return ("{" +
-                "\n id: " + self.id +
-                ",\n idade: " + self.idade +
-                ",\n altura: " + self.altura +
-                ",\n tecnica: " + self.tecnica +
-                ",\n passe: " + self.passe +
-                ",\n chute: " + self.chute +
-                ",\n forca: " + self.forca +
-                ",\n velocidade: " + self.velocidade +
-                ",\n drible: " + self.drible +
-                ",\n classe: " + (self.classe or 'none') +
-                "\n}")
 
 
 def readFile(filename):
     arr = []
     with open(filename, newline='') as arquivo:
-        dados = csv.reader(arquivo, delimiter=';', skipinitialspace=True)
+        dados = csv.reader(arquivo, delimiter=';')
         next(dados)
         for linha in dados:
-            arr.append(Jogador(linha))
+            arr.append(linha)
     return arr
 
 
+def criar_dataset(dados, inicio, fim=None):
+    if(fim is None):
+        dados = dados[inicio:len(dados)]
+    else:
+        dados = dados[inicio:fim]
+
+    dados_x = list(map(lambda x: x[3:9], dados))
+    dados_y = list(map(lambda x: 1 if(x[9] == 'Atacante') else 0, dados))
+
+    return tf_set(dados_x, dados_y)
+
+
+def preprocess(x, y):
+    x = tf.cast(x, tf.float32) / 100
+    return x, y
+
+
+def tf_set(xs, ys, n_classes=2):
+    ys = tf.one_hot(ys, depth=n_classes)
+    return tf.data.Dataset.from_tensor_slices((xs, ys)) \
+        .map(preprocess) \
+        .shuffle(len(ys)) \
+        .batch(128)
+
+
 jogadores = readFile('datasets/jogadores.csv')
-jogadores_ideais = readFile('datasets/validacao.csv')
+jogadores_resultantes = readFile('datasets/validacao.csv')
+N_TREINO = int(len(jogadores)*80/100)
+
+jogadores_treino = criar_dataset(jogadores, 0, N_TREINO)
+jogadores_teste = criar_dataset(jogadores, N_TREINO)
+
+print(jogadores_treino)
